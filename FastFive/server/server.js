e69974a132fdde63a4b45315cd03a33a33fae44a
misoh <misohongsik@gmail.com>
2025-11-20 23:55:01 +0900

    Downgrade Express to v4 for catch-all route compatibility

--- FastFive/server/server.js
+++ FastFive/server/server.js
@@ -73,7 +73,7 @@ app.patch('/api/admin/status', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
